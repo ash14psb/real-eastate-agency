5c5cc2b076749fd33d0599b01c167785859b97c2
ash14psb <crtarik420@gmail.com>
2023-07-06 11:55:05 +0600

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <button className="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
+          Button
+        </button>
+      </div>
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <figure>
+          <img
+            src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+            alt="Shoes"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">Shoes!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
